7e198e8f27915760682545f2833a31c90ab6112e
JustJokerX <jiajun.xie.sh@gmail.com>
2016-12-27 19:56:24 +0800

    python code with CVPR and NIPS for several years

---
+++ CVPR/CVPR2014.py
@@ -0,0 +1,35 @@
+#coding=utf-8
+"""
+This file is used to make a crawl
+"""
+import re
+import urllib
+import os
+
+def get_html(url):
+    """Get the html """
+    page = urllib.urlopen(url)
+    html = page.read()
+    return html
+
+def get_pdf(html):
+    """ xxx"""
+    reg = r'href="(.+?\.pdf)">pdf'
+    pdfre = re.compile(reg)
+    pdflist = re.findall(pdfre, html)
+    dir_name = 'CVPR2014'
+    if os.path.exists(dir_name) is False:
+        os.mkdir(dir_name)
+    for pdfurl in pdflist:
+        reg2 = r'papers/(.+?\.pdf)'
+        pdfre2 = re.compile(reg2)
+        filename = dir_name + '/'+ re.findall(pdfre2, pdfurl)[0]
+        print 'http://www.cv-foundation.org/openaccess/'+pdfurl
+        if os.path.exists(filename) is True:
+            print 'Exist'
+        else:
+            urllib.urlretrieve('http://www.cv-foundation.org/openaccess/'+pdfurl, filename)
+
+
+HTML = get_html("http://www.cv-foundation.org/openaccess/CVPR2014.py")
+print get_pdf(HTML)
---
+++ NIPS/NIPS2014.py
@@ -0,0 +1,34 @@
+#coding=utf-8
+"""
+This file is used to make a crawl
+"""
+import re
+import urllib
+import os
+
+def get_html(url):
+    """Get the html """
+    page = urllib.urlopen(url)
+    html = page.read()
+    return html
+
+def get_pdf(html):
+    """ xxx"""
+    reg = r'href="/paper/(.+?)"'
+    pdfre = re.compile(reg)
+    pdflist = re.findall(pdfre, html)
+    dir_name = 'NIPS2014'
+    if os.path.exists(dir_name) is False:
+        os.mkdir(dir_name)
+    for pdfurl in pdflist:
+        filename = dir_name+'/'+ pdfurl+'.pdf'
+        print 'http://papers.nips.cc/paper/'+pdfurl+'.pdf'
+        if os.path.exists(filename) is True:
+            print 'Exist'
+        else:
+            urllib.urlretrieve('http://papers.nips.cc/paper/'+pdfurl+'.pdf', filename)
+
+
+HTML = get_html('http://papers.nips.cc/book/'
+                'advances-in-neural-information-processing-systems-27-2014')
+print get_pdf(HTML)
